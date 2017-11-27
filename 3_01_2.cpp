857dc1386b8e88bbfb0cfe1e14b2892f32fa002f
vitek2109 <vitek21098@gmail.com>
2017-11-27 21:35:19 +0300

    renaming (e.g. 3_1_1 -> 3_01_1)

---
+++ 3_01_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using std::cout; using std::endl;
+
+int main()
+{
+    int i = 10;
+    while (i >= 0)
+        cout << i-- << endl;
+}
---
+++ 3_01_5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using std::cin; using std::cout; using std::endl; using std::string;
+
+struct Sales_data {
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+int main()
+{
+    Sales_data data1, data2;
+    double price1, price2;
+    cin >> data1.bookNo >> data1.units_sold >> price1;
+    data1.revenue = data1.units_sold * price1;
+    while (cin >> data2.bookNo)
+    {
+        cin >> data2.units_sold >> price2;
+        data2.revenue = data2.units_sold * price2;
+        data1.units_sold += data2.units_sold;
+        data1.revenue += data2.revenue;
+    }
+    cout << data1.bookNo << " " << data1.units_sold << " " << data1.revenue/data1.units_sold << endl;
+}
