02455b5699e84577bbec0d7b6a3c5c3b1b9a49a7
Viktor <vitek21098@gmail.com>
2018-01-27 11:54:59 +0300

    4_17 to 4_19 added

---
+++ 4_19.cpp
@@ -0,0 +1,10 @@
+// (a)
+// ptr != 0 && *ptr++
+// Tbh I can't figure out what is supposed to happen here
+// (ptr != 0) && *ptr++ // maybe like that
+//
+// (b)
+// ival + 1 && ival
+//
+// (c)
+// vec[ival + 1] <= vec[ival]
