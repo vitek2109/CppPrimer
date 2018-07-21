ad703dde2a8a3a8f8100e95df174ff721185c053
Viktor <vitek21098@gmail.com>
2018-07-21 19:18:24 +0300

    New file added

---
+++ Chapter 11/11_33.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
+#include <exception>
+
+std::map<std::string, std::string> make_map (std::ifstream &rules) {
+    std::map<std::string, std::string> res;
+    std::string key, value;
+    while (rules >> key && std::getline(rules, value)) {
+        if (value.size() > 1)
+            res[key] = value.substr(1);
+        else
+            throw std::runtime_error("no rule for " + key);
+    }
+    return res;
+}
+
+const std::string &transform (const std::string &word, const std::map<std::string, std::string> &rules) {
+    auto map_it = rules.find(word);
+    if (map_it != rules.cend())
+        return map_it->second;
+    else
+        return word;
+}
+
+void word_process (std::ifstream &rules, std::ifstream &text) {
+    auto rule_map = make_map(rules);
+    std::string line;
+    while (std::getline(text, line)) {
+        std::istringstream line_stream(line);
+        std::string word;
+        while (line_stream >> word) {
+            std::cout << transform(word, rule_map) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    std::ifstream rules("infile1"), text("infile2");
+    word_process(rules, text);
+    rules.close();
+    text.close();
+}
