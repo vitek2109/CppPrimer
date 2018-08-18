4f8c30ed7d4cfd726ef52533af50577cb57ebebc
Viktor <vitek21098@gmail.com>
2018-08-18 14:22:38 +0300

    New file added

---
+++ Chapter 13/13_8.cpp
@@ -0,0 +1,17 @@
+#include <string>
+int main() { }
+
+class HasPtr {
+public:
+    HasPtr (const std::string &s = std::string()) :
+            ps(new std::string(s)), i(0) { }
+    HasPtr (const HasPtr &orig) :
+            ps(new std::string(*orig.ps)), i(orig.i) { }
+    HasPtr& operator= (const HasPtr &rhs) {
+        *ps = *rhs.ps;
+        i = rhs.i; 
+    }
+private:
+    std::string *ps;
+    int i;
+};
