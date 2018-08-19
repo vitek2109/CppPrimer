e1e24610e1725157282d624fb6e7a44e3b5926d6
Viktor <vitek21098@gmail.com>
2018-08-20 01:06:51 +0300

    Made them look nicer

--- Chapter 12/StrBlob.cpp
+++ Chapter 12/StrBlob.cpp
@@ -2,75 +2,100 @@
 
 typedef std::vector<std::string>::size_type size_type;
 
+// ----------------------------StrBlob Implementation----------------------------
+
+// constructors
 StrBlob::StrBlob() : data(std::make_shared<std::vector<std::string>>()) { }
 StrBlob::StrBlob(std::initializer_list<std::string> il) : data(std::make_shared<std::vector<std::string>>(il)) { }
-
-size_type StrBlob::size() const { 
-    return data->size(); 
-}
-
-bool StrBlob::empty() const {
-    return data->empty(); 
-}
-
-void StrBlob::push_back(const std::string &t) {
-    data->push_back(t); 
-}
-
-StrBlobPtr StrBlob::begin() {
-    return StrBlobPtr (*this);
-}
-
-StrBlobPtr StrBlob::end() {
-    return StrBlobPtr (*this, data->size());
-}
 
 void StrBlob::check (size_type i, const std::string &msg) const {
     if (i >= data->size())
         throw std::out_of_range(msg);
 }
 
+// element access
+std::string& StrBlob::front() {
+    check (0, "front() on empty StrBlob");
+    return data->front();
+}
+std::string& StrBlob::back() {
+    check (0, "back() on empty StrBlob");
+    return data->back();
+}
+// const versions
+const std::string& StrBlob::front() const {
+    check (0, "front() on empty StrBlob");
+    return data->front();
+}
+const std::string& StrBlob::back() const {
+    check (0, "back() on empty StrBlob");
+}
+
+// element revomal
 void StrBlob::pop_back() {
-    check(0, "pop_back");
+    check(0, "pop_back() on empty StrBlob");
     data->pop_back();
 }
 
-std::string& StrBlob::front() {
-    check(0, "front");
-    return data->front();
+StrBlobPtr StrBlob::begin() {
+    return StrBlobPtr(*this);
+}
+StrBlobPtr StrBlob::end() {
+    return StrBlobPtr(*this, data->size());
+}
+ConstStrBlobPtr StrBlob::cbegin() {
+    return ConstStrBlobPtr(*this);
+}
+ConstStrBlobPtr StrBlob::cend() {
+    return ConstStrBlobPtr(*this, data->size());
 }
 
-const std::string& StrBlob::front() const {
-    check(0, "front");
-    return data->front();
-}
 
-std::string& StrBlob::back() {
-    check(0, "back");
-    return data->back();
-}
+// ----------------------------StrBlobPtr Implementation----------------------------
 
-const std::string& StrBlob::back() const {
-    check(0, "back");
-    return data->back();
-}
-
-std::shared_ptr<std::vector<std::string>> StrBlobPtr::check(size_t i, const std::string &msg) const {
-    auto ret = wptr.lock();
+std::shared_ptr<std::vector<std::string>> StrBlobPtr::check(std::size_t i, const std::string &msg) const {
+    auto ret = wptr.lock(); // does vector still exist
     if (!ret)
-        throw std::runtime_error("Dangling weak_ptr");
+        throw std::runtime_error("Unbound StrBlobPtr");
     if (i >= ret->size())
         throw std::out_of_range(msg);
-    return ret;
+    return ret; // else return shared_ptr to vector
 }
 
+// pointer operations
 std::string& StrBlobPtr::deref() const {
-    auto p = check(curr, "deref");
-    return (*p)[curr];
+    auto p = check(curr, "dereference past end");
+    return (*p)[curr]; // (*p) is the vector, which this object points to
+}
+// prefix: return reference to this object after increment
+StrBlobPtr& StrBlobPtr::incr() {
+    // if curr is at the end, then throw an exeption
+    check(curr, "increment past end of StrBlobPtr");
+    ++curr;
+    return *this;
 }
 
-StrBlobPtr& StrBlobPtr::incr() {
-    check(curr, "incr");
+
+// ----------------------------ConstStrBlobPtr Implementation----------------------------
+
+std::shared_ptr<std::vector<std::string>> ConstStrBlobPtr::check(std::size_t i, const std::string &msg) const {
+    auto ret = wptr.lock(); // does vector still exist
+    if (!ret)
+        throw std::runtime_error("Unbound StrBlobPtr");
+    if (i >= ret->size())
+        throw std::out_of_range(msg);
+    return ret; // else return shared_ptr to vector
+}
+
+// pointer operations
+std::string& ConstStrBlobPtr::deref() const {
+    auto p = check(curr, "dereference past end");
+    return (*p)[curr]; // (*p) is the vector, which this object points to
+}
+// prefix: return reference to this object after increment
+ConstStrBlobPtr& ConstStrBlobPtr::incr() {
+    // if curr is at the end, then throw an exeption
+    check(curr, "increment past end of StrBlobPtr");
     ++curr;
     return *this;
 }
--- Chapter 12/StrBlob.h
+++ Chapter 12/StrBlob.h
@@ -3,27 +3,37 @@
 #include <memory>
 #include <vector>
 #include <string>
-#include <exception>
 
 class StrBlobPtr;
+class ConstStrBlobPtr;
+
 class StrBlob {
-public:
     friend class StrBlobPtr;
+    friend class ConstStrBlobPtr;
+public:
     typedef std::vector<std::string>::size_type size_type;
     StrBlob();
     StrBlob(std::initializer_list<std::string> il);
-    size_type size() const;
-    bool empty() const;
-    void push_back(const std::string &t);
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+    // addition and removal of elements
+    void push_back(const std::string &t) { data->push_back(t); }
     void pop_back();
+    // element access
     std::string& front();
-    const std::string& front() const ;
     std::string& back();
-    const std::string& back() const ;
+    // const versions
+    const std::string& front() const;
+    const std::string& back() const;
+    // return pointers to the first and after-last elements of the vector
     StrBlobPtr begin();
     StrBlobPtr end();
+    // const versions
+    ConstStrBlobPtr cbegin();
+    ConstStrBlobPtr cend();
 private:
     std::shared_ptr<std::vector<std::string>> data;
+    // throw an exeption if data[i] is not accessible
     void check (size_type i, const std::string &msg) const;
 };
 
@@ -31,12 +41,30 @@ class StrBlobPtr {
 public:
     StrBlobPtr() : curr(0) { }
     StrBlobPtr(StrBlob &a, size_t sz = 0) : wptr(a.data), curr(sz) { }
+    // pointer operations
     std::string& deref() const;
-    StrBlobPtr& incr();
+    StrBlobPtr& incr(); // prefix increment
 private:
-    std::shared_ptr<std::vector<std::string>> check(size_t i, const std::string&) const;
+    // check() returns shared_ptr to vector only if everything is fine
+    std::shared_ptr<std::vector<std::string>> check(std::size_t i, const std::string &msg) const;
+    // stores weak_ptr, so main vector can be deleted
     std::weak_ptr<std::vector<std::string>> wptr;
-    std::size_t curr;
+    std::size_t curr; // curent position inside the vector
+};
+
+class ConstStrBlobPtr {
+public:
+    ConstStrBlobPtr() : curr(0) { }
+    ConstStrBlobPtr(const StrBlob &a, size_t sz = 0) : wptr(a.data), curr(sz) { }
+    // pointer operations
+    std::string& deref() const;
+    ConstStrBlobPtr& incr(); // prefix increment
+private:
+    // check() returns shared_ptr to vector only if everything is fine
+    std::shared_ptr<std::vector<std::string>> check(std::size_t i, const std::string &msg) const;
+    // stores weak_ptr, so main vector can be deleted
+    std::weak_ptr<std::vector<std::string>> wptr;
+    std::size_t curr; // curent position inside the vector
 };
 
 #endif
