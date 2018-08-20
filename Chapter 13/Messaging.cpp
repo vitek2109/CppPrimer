54ad5c61481c568e92920c5f3f38c0edc5a8738a
Viktor <vitek21098@gmail.com>
2018-08-20 23:41:34 +0300

    New Files Added

---
+++ Chapter 13/Messaging.cpp
@@ -0,0 +1,111 @@
+#include "Messaging.h"
+#ifdef DEBUG
+int main() { }
+#endif
+
+// -------------------------Message implementation-------------------------
+// copy control
+Message::Message(const Message &m) : 
+    contents(m.contents), folders(m.folders)
+{
+    add_to_Folders(m);
+}
+Message& Message::operator= (const Message &rhs)
+{
+    remove_from_Folders();      // remove this message from all folders
+    contents = rhs.contents;    // copy everything over from rhs
+    folders = rhs.folders;
+    add_to_Folders(rhs);        // add this message to all of it's new folders
+    return *this;
+}
+Message::~Message()
+{
+    remove_from_Folders();
+}
+// add/delete this message to/from a set of messages of the folder
+void Message::save(Folder &f)
+{
+    folders.insert(&f); // add f to folders
+    f.addMsg(*this);     // add this message to f's set
+}
+void Message::remove(Folder &f)
+{
+    folders.erase(&f);  // remove f from folders
+    f.remMsg(*this);     // remove this message form f's set
+}
+// private functions
+// add this message to all folders of some other message
+void Message::add_to_Folders(const Message &m)
+{
+    for (auto f : m.folders)    // for every folder containing m
+        f->addMsg(*this);        // add this message to it
+}
+// remove this message from all folders
+void Message::remove_from_Folders()
+{
+    for (auto f : folders)  // for every folder
+        f->remMsg(*this);    // remove this message
+}
+// swap
+void swap(Message &lhs, Message &rhs)
+{
+    using std::swap;                    // unnecessary, but a good habit
+    lhs.remove_from_Folders();          // remove all pointers to these messages
+    rhs.remove_from_Folders();
+    swap(lhs.contents, rhs.contents);   // swap everything
+    swap(lhs.folders, rhs.folders);
+    lhs.add_to_Folders(lhs);            // add pointers from all the new folders
+    rhs.add_to_Folders(rhs);
+}
+
+// -------------------------Message implementation-------------------------
+// copy control
+Folder::Folder(const Folder &f) : messages(f.messages)
+{
+    add_to_Messages(f);
+}
+Folder& Folder::operator= (const Folder &rhs)
+{
+    remove_from_Messages();     // remove all pointers to this folder
+    messages = rhs.messages;    // copy everything
+    add_to_Messages(rhs);          // add this folder to all of it's new messages
+    return *this;
+}
+Folder::~Folder()
+{
+    remove_from_Messages();
+}
+// message manipulation
+Folder::addMsg(Message &m)
+{
+    messages.insert(&m);
+    m.save(*this);
+}
+Folder::remMsg(Message &m)
+{
+    messages.erase(&m);
+    m.remove(*this);
+}
+// private functions
+// add all messages from other folder
+void Folder::add_to_Messages(const Folder &f)
+{
+    for (auto m : f.messages)
+        m->save(*this);
+}
+//remove all messages
+void Folder::remove_from_Messages()
+{
+    for (auto m : messages)
+        m->remove(*this);
+}
+// swap
+void swap(Folder &lhs, Folder &rhs)
+{
+    using std::swap;                    // unnecessary, but a good habit
+    lhs.remove_from_Messages();          // remove all pointers to these folders
+    rhs.remove_from_Messages();
+    swap(lhs.messages, rhs.messages);   // swap everything
+    lhs.add_to_Messages(lhs);           // add pointers from all the new messages
+    rhs.add_to_Messages(rhs);
+}
---
+++ Chapter 13/Messaging.h
@@ -0,0 +1,56 @@
+#ifndef MESSAGING
+#define MESSAGING
+#include <string>
+#include <set>
+
+class Folder;
+
+class Message {
+    friend class Folder;
+    friend void swap (Message&, Message&);
+public:
+    // folders implicitly initializes as an empty set 
+    explicit Message(const std::string &s = "") : contents(s) {}
+    // copy control
+    Message(const Message&);                // copy constructor
+    Message& operator= (const Message&);    // copy-assignment operator
+    ~Message();                             // destructor
+    // add/delete this message to/from a set of messages of the folder
+    void save(Folder&);
+    void remove(Folder&);
+private:
+    std::string contents;       // text of this message
+    std::set<Folder*> folders;   // all the folders that contain this message
+    // a set of functions used by other methods
+    // add this message to all folders of some other message
+    void add_to_Folders(const Message&);
+    // remove this message from all folders
+    void remove_from_Folders();
+};
+// swap two messages
+void swap (Message&, Message&);
+
+class Folder {
+    friend class Message;
+    friend void swap(Folder&, Folder&);
+public:
+    Folder() { }
+    // copy control
+    Folder(const Folder&);              // copy constructor
+    Folder& operator= (const Folder&);  // copy-assignment operator
+    ~Folder();                          // destructor
+    // message manipulation
+    addMsg(Message&); // add message to this folder
+    remMsg(Message&); // remove message from this folder
+private:
+    std::set<Message*> messages; // messages this folder contains
+    // a set of functions used by other methods
+    // add all messages from other folder
+    void add_to_Messages(const Folder&);
+    //remove all messages
+    void remove_from_Messages();
+};
+// swap two folders
+void swap(Folder&, Folder&);
+
+#endif
