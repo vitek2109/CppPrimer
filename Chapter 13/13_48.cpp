76856b56629603702a194577d9aaf4cb9b1eadb0
Viktor <vitek21098@gmail.com>
2018-08-22 16:42:28 +0300

    New File Added

---
+++ Chapter 13/13_48.cpp
@@ -0,0 +1,22 @@
+#include "String.h"
+#include <vector>
+
+int main()
+{
+    std::vector<String> v;
+    v.push_back("Hello, World!");
+    v.push_back("42");
+    v.push_back("42");
+    v.push_back("42");
+    v.push_back("42");
+    v.push_back("Hello, World!");
+    v.push_back("42");
+    v.push_back("42");
+    v.push_back("42");
+    v.push_back("42");
+    v.push_back("Hello, World!");
+    v.push_back("42");
+    v.push_back("42");
+    v.push_back("42");
+    v.push_back("42");
+}
